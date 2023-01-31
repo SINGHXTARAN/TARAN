0330525e7296626ee5df65c883403d33e15c7d2b
taranvir singh <taranvir singh@10.48.19.126>
2023-01-31 14:25:06 -0500

    day1

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,17 @@
+
+package helloworld;
+
+/**
+ *
+ * @author taranvir singh
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+    
+}
